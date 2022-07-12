c9fa1ad89023c3f0964de08264a942811569ce60
Quang huy <2hnono30@gmail.com>
2022-07-12 16:38:45 +0700

    computer

---
+++ maytinh.html/computer.js
@@ -0,0 +1,25 @@
+
+function add() {
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+    let result = number(number1) + number(number2);
+    document.getElementById("result").innerHTML= result;
+}
+function add() {
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+    let result = number(number1) - number(number2);
+    document.getElementById("result").innerHTML= result;
+}
+function add() {
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+    let result = number(number1) * number(number2);
+    document.getElementById("result").innerHTML= result;
+}
+function add() {
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+    let result = number(number1) / number(number2);
+    document.getElementById("result").innerHTML= result;
+}
